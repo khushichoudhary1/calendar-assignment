c705e89d33f422e71296f1aaf9abf90db4b65566
Khushi <choudharykhushi7906@gmail.com>
2025-11-01 20:24:26 +0530

    Fix: exclude stories and unused React imports for Vercel build

--- src/components/Calendar/CalendarView.stories.tsx
+++ src/components/Calendar/CalendarView.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
+
 import CalendarView from "./CalendarView";
 import type { CalendarEvent } from "./CalendarView.types";
 
